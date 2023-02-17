8590fa1895a84c095f1bf8cca0255ef738cfd7ba
Sudipta Pramanik <sudiptapramanik209@gmail.com>
2023-02-17 21:57:22 +0530

    files add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./components/section/About";
 import Works from "./components/section/Works";
 import Contact from "./components/section/Contact";
 import AOS from "aos";
-//import "aos/dist/aos.css";
+import "aos/dist/aos.css";
 import { animation } from "./profile";
 
 function App() {
